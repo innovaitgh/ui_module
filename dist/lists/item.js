55a896c939d578448902ad964f4673706f673096
Kwame Shyd <kwameshyd@gmail.com>
2020-11-01 11:39:30 +0000

    removed onDelete

--- dist/lists/item.js
+++ dist/lists/item.js
@@ -7,34 +7,18 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _with_async_action_confirm_dialog = _interopRequireDefault(require("with_async_action_confirm_dialog"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-var Item = function Item(props) {
+var _default = function _default(props) {
   var RenderItem = props.RenderItem,
       item = props.item,
-      removeItem = props.removeItem,
-      onClick = props.onClick,
-      withAsyncActionResult = props.withAsyncActionResult;
-
-  _react["default"].useEffect(function () {
-    if (!withAsyncActionResult) return;
-    var status = withAsyncActionResult.status;
-    if (status === 200) onDelete();
-  }, [withAsyncActionResult]);
-
-  var onDelete = function onDelete() {
-    return removeItem(item);
-  };
-
+      index = props.index;
   return _react["default"].createElement(RenderItem, _extends({}, props, {
-    onDelete: onClick
+    item: item,
+    index: index
   }));
 };
-
-var _default = (0, _with_async_action_confirm_dialog["default"])(Item);
 
 exports["default"] = _default;
--- src/navigation/current_user_nav/index.js
+++ src/navigation/current_user_nav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import IconButton from "@material-ui/core/IconButton";
 import Avatar from "@material-ui/core/Avatar";
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Go from "../go";
 
 const avatarStyles = makeStyles({
